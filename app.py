9c38c46a909bcd10871de50b8e4ae0f8559fcc79
Saloni Agarwal <saloni.bitspilani@gmail.com>
2024-04-02 12:31:12 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from src.performanceprediction.logger import logging
 from src.performanceprediction.exception import CustomException
+from src.performanceprediction.components.data_ingestion import DataIngestion
 import sys
 
 if __name__=="__main__":
     logging.info("Execution started")
 
     try:
-        a=1/0
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
---
+++ src/performanceprediction/utils.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from src.performanceprediction.exception import CustomException
+from src.performanceprediction.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv("db")
+
+
+def read_sql_data():
+    logging.info('Reading SQL database started')
+    try:
+        mydb=pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info('Connection done', mydb)
+        df=pd.read_sql_query('Select * from student', mydb)
+        print(df.head())
+        return df
+
+    except Exception as e:
+        raise CustomException(e,sys)
