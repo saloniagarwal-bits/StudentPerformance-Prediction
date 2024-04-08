165acf98b0ee9eed1a9cf88e7dfbdab839f6ddcb
Saloni Agarwal <saloni.bitspilani@gmail.com>
2024-04-09 00:30:04 +0530

    Added Prediction Pipeline

--- src/performanceprediction/utils.py
+++ src/performanceprediction/utils.py
@@ -75,3 +75,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
     except Exception as ex:
         raise CustomException(ex, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
